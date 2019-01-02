44a060ca1d51fb7b14960e907f0054ffbdd23770
Simon Andersen <andersen.simon.c@gmail.com>
2019-01-02 22:06:48 +0000

    [SRC] Added an utility function to inspect wrong answers

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+from src.evaluate import emotion2label, label2emotion
+import numpy as np
+import pandas as pd
+
+
 def reindex_sequences(seq_samples, src_word_index, dst_word_index):
     src_index_word = {v: k for k, v in src_word_index.items()}
     for seq in seq_samples:
@@ -10,3 +15,28 @@ def reindex_sequences(seq_samples, src_word_index, dst_word_index):
 def check_mode(mode):
     if mode not in ['categorical', 'binary', 'ensemble']:
         raise ValueError("mode argument must be 'categorical', 'binary' or 'ensemble'")
+
+
+def get_wrongs(y_proba, targets, texts, emotion=None):
+    if emotion:
+        emo_idx = emotion2label[emotion]
+
+    y_pred = y_proba.argmax(axis=1)
+    y_targets = targets.argmax(axis=1)
+
+    emotions_targets = np.vectorize(lambda x: label2emotion[x])(y_targets)
+    emotions_predicted = np.vectorize(lambda x: label2emotion[x])(y_pred)
+
+    txt_probas = [str(x) for x in y_proba]
+    df = pd.DataFrame({'Predicted': emotions_predicted,
+                       'Actual': emotions_targets,
+#                        'Probas': txt_probas,
+                       'Text': texts.apply(lambda x: ' '.join(x)),
+                      })
+
+    if emotion:
+        wrongs = np.where((y_pred != y_targets) & (y_pred == emo_idx))
+    else:
+        wrongs = np.where(y_pred != y_targets)
+
+    return df.loc[wrongs]
